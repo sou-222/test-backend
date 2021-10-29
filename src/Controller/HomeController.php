a9e065c3f17383fca6507d290d25af754694bec3
Val <noreply@soulenq.eu>
2021-10-29 10:08:35 +0200

    NEW [Task-5] feature: Add a new route (/tv/fr|en) which return in json the channel names sorted by num, and filtered by language.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Channel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class HomeController extends AbstractController
 {
@@ -16,5 +21,19 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    /**
+     * @Route("/tv/{language}")
+     */
+    public function channels(string $language): JsonResponse
+    {
+        $channels = $this->getDoctrine()
+            ->getRepository(Channel::class)
+            ->findBy(['language'=>$language], ['id' => 'ASC']);
+
+        $serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
+        $jsonContent = $serializer->serialize($channels, 'json');
+        return JsonResponse::fromJsonString($jsonContent);
     }
 }
